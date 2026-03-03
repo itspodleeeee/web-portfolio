c5f8f63e137abbc7e7d2405990fec6a2b155a763
ㅤCryptX <johnbotin321@gmail.com>
2026-03-03 21:02:08 +0800

    add tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
   description:
     "Entry-level IT support and cybersecurity-focused BSIT student with real-world content operations and analytics experience.",
   metadataBase: new URL("https://example.com"),
+  icons: {
+    icon: "https://camo.githubusercontent.com/e8d7a0f9b14ae56c0d95fe8db0e653db814a82d747c2cbdf65444d01dac10230/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3132382f333937352f333937353039302e706e67"
+  },
   openGraph: {
     title: "John Wilberth B. Botin | Portfolio",
     description:
