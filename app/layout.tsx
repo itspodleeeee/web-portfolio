5f2bdbd8377043f3df76269acb65c5a355bee1c1
ㅤCryptX <johnbotin321@gmail.com>
2026-03-04 08:23:48 +0800

    Add vercel insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ThemeProvider } from "../components/ThemeProvider";
 
@@ -36,6 +38,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans`}>
         <ThemeProvider>{children}</ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
